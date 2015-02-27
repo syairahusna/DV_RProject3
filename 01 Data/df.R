876dc7ce08a1ebad609f028d27d73effb251c7a8
Syairah <syairah@wireless-10-145-41-237.public.utexas.edu>
2015-02-27 13:20:37 -0600

    still can't pull full dataset from wadi

--- 01 Data/df.R
+++ 01 Data/df.R
@@ -8,13 +8,13 @@ require(dplyr)
 # show the dataframes' stuctures
 a <- 'Armenia'
 
-hnp_sample_df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from hnp where country_name = \\\'"a"\\\'  "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', a=a, returnFor = 'JSON'), verbose = TRUE)))
+hnp_sample_df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from hnp "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
 tbl_df(hnp_sample_df)
 
 b <- 'Kazakhstan'
 
-wdi_sample_df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from hnp where country_name = \\\'"q"\\\'"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', q=b, returnFor = 'JSON'), verbose = TRUE)))
+wdi_sample_df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from wdi "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
 tbl_df(wdi_sample_df)
 
