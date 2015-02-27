b5d57aab949383885210a06bbc305050eaaa6bbe
Syairah <syairah@wireless-10-145-41-237.public.utexas.edu>
2015-02-27 14:50:22 -0600

    all datasets and joins ready to use

--- 01 Data/df.R
+++ 01 Data/df.R
@@ -6,31 +6,23 @@ require(tidyr)
 require(dplyr)
 
 # show the dataframes' stuctures
-a <- 'Armenia'
 
-hnp_sample_df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from hnp "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+hnp_df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from hnp "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
-tbl_df(hnp_sample_df)
+tbl_df(hnp_df)
 
-b <- 'Kazakhstan'
 
-wdi_sample_df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from wdi "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+wdi_df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from wdi "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
-tbl_df(wdi_sample_df)
+tbl_df(wdi_df)
 
 #inner join
-c <- 'Malaysia'
-hnp_msia_df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from hnp where country_name = \\\'"a"\\\'  "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', a=c, returnFor = 'JSON'), verbose = TRUE)))
 
-wdi_msia_df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from wdi where country_name = \\\'"a"\\\'  "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_nm22335', PASS='orcl_nm22335', MODE='native_mode', MODEL='model', returnDimensions = 'False', a=c, returnFor = 'JSON'), verbose = TRUE)))
-
-
-
-d <- inner_join(wdi_msia_df, hnp_msia_df)               
-                
+d <- inner_join(wdi_df, hnp_df)
+                  
 d %>% tbl_df()
 
-f <- full_join(wdi_msia_df, hnp_msia_df)
+f <- full_join(wdi_df, hnp_df)
 
 f %>% tbl_df()
 
@@ -38,7 +30,7 @@ g <- semi_join(hnp_msia_df, wdi_msia_df)
 
 g %>% tbl_df()
 
-h <- anti_join(wdi_msia_df, hnp_msia_df)
+h <- anti_join(hnp_df, wdi_df)
 
 h %>% tbl_df()
 
