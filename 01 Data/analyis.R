4b2dc4d76f0fa1a633a2da456561ca63ce5d5e68
Syairah <syairah@Nurs-MacBook-Pro.local>
2015-03-04 12:25:15 -0600

    analysis done

--- 01 Data/analyis.R
+++ 01 Data/analyis.R
@@ -694,9 +694,9 @@ View(unique_nonio)
 
 #1
 #getting both on top of each other
-#p <- gather(unique_io, "rates", "value", 4:6) 
-#q <- gather(unique_nonio, "rates", "value", 4:6)
-#ggplot() + geom_violin(data = p, aes(x = rates, y=value), fill = "red", alpha = 0.5) + geom_violin(data = q, aes(x = rates, y=value), fill = "yellow", alpha = 0.5) + coord_flip() + ggtitle('Electricity Rates For Zipcodes With Only Either Investor Owned Utilities or Non-Investor Owned Utilities') 
+p <- gather(unique_io, "rates", "value", 4:6) 
+q <- gather(unique_nonio, "rates", "value", 4:6)
+ggplot() + geom_violin(data = p, aes(x = rates, y=value), fill = "red", alpha = 0.5) + geom_violin(data = q, aes(x = rates, y=value), fill = "yellow", alpha = 0.5) + coord_flip() + ggtitle('Electricity Rates For Zipcodes With Only Either Investor Owned Utilities or Non-Investor Owned Utilities') 
 
 p <- gather(unique_io, "rates", "value", 4:6) 
 q <- gather(unique_nonio, "rates", "value", 4:6)
